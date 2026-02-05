6456a25260de4c206f463da666e755ab2698a4bb
Emily Cardona <ecardonac2@eafit.edu.co>
2026-02-05 11:36:41 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse("<h1>Welcome Homepage</h1>")
     #return render(request, "home.html")
-     return render(request, "home.html", {"name" : "Greg lim"})
+     return render(request, "home.html", {"name" : "Emily Cardona"})
 
 def About(request):
     return HttpResponse("<h1>Welcome About</h1>")
