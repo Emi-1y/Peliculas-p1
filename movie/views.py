60f6e177d1346fe1b676ed50865f2a9f21dc76dd
Emily Cardona <ecardonac2@eafit.edu.co>
2026-02-05 11:16:06 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    #return HttpResponse("<h1>Welcome Homepage</h1>")
+    #return render(request, "home.html")
+     return render(request, "home.html", {"name" : "Greg lim"})
+
+def About(request):
+    return HttpResponse("<h1>Welcome About</h1>")
+
+
+# Create your views here.
