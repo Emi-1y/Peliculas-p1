ffba7d2f4bbbec6cc3b2e11022af650717d24f0d
Emily Cardona <ecardonac2@eafit.edu.co>
2026-02-08 20:48:49 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
 
-def home(request):
     #return HttpResponse("<h1>Welcome Homepage</h1>")
     #return render(request, "home.html")
-     return render(request, "home.html", {"name" : "Emily Cardona"})
+    # return render(request, "home.html", {"name" : "Emily Cardona"})
 
-def About(request):
-    return HttpResponse("<h1>Welcome About</h1>")
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+
+def about(request):
+   return HttpResponse("<h1>Welcome About</h1>")
 
 
 # Create your views here.
